4cbed78989c81a3d17b9272e2037ae9dc3dfdfea
Gabriel Note <gabriel.emil.note@gmail.com>
2025-08-29 12:15:12 +0200

    Pushing to github

--- src/Main.java
+++ src/Main.java
@@ -25,16 +25,16 @@ public class Main {
         //övning 1.3
 
         String format1_3 = "%-24s%s%n";
-        String name = "Gabriel Note";
-        int age = 31;
+        String name1_3 = "Gabriel Note";
+        int age1_3 = 31;
         int numberOfSiblings = 2;
         String favoriteFood = "Ramen";
         double height = 1.86;
         boolean iLikeCoffe = false;
         System.out.println("lite om mig");
-        System.out.printf(format1_3, "Namn: ",name);
+        System.out.printf(format1_3, "Namn: ",name1_3);
         System.out.printf(format1_3, "Favoritmat: ",favoriteFood);
-        System.out.printf(format1_3, "Ålder: ",age);
+        System.out.printf(format1_3, "Ålder: ",age1_3);
         System.out.printf(format1_3, "Antal syskon: ",numberOfSiblings);
         System.out.printf(format1_3, "Höjd: ",height + " m");
         System.out.printf(format1_3, "Jag gillar kaffe: ",iLikeCoffe);
@@ -43,13 +43,13 @@ public class Main {
         //Övning 2.1
 
         System.out.println("Skriv ert namn: ");
-        String name1 = scanner.nextLine();
+        String name2_1 = scanner.nextLine();
         System.out.println("Skriv er ålder: ");
-        String age1 = scanner.nextLine();
+        String age2_1 = scanner.nextLine();
         System.out.println("Skriv var ni bor: ");
         String placeOfResidence = scanner.nextLine();
-        System.out.println("Hej " + name1 + " välkommen till " + placeOfResidence + "! (trotts att ni är " + age1 + " år gammal.");
-        */
+        System.out.println("Hej " + name2_1 + " välkommen till " + placeOfResidence + "! (trotts att ni är " + age2_1 + " år gammal.");
+
 
         // Övning 2.2
 
@@ -66,11 +66,41 @@ public class Main {
         System.out.printf(format2_2, "Summan: ",sum);
         System.out.printf(format2_2, "Produkten: ",product);
         System.out.printf(format2_2, "Genomsnittet: ",average);
+        System.out.println(" ");
 
 
         //Övning 2.3
 
+        String format2_3 = "%-15s%s%n";
 
+        System.out.println("lokal valutakonverterare från SEK till EUR och USD");
+        System.out.println("Ange ett belopp i SEK som ni vill konvertera");
+        float money = scanner.nextFloat();
+        float euro = money * 0.085f;
+        float dollar = money * 0.091f;
+        System.out.println(money + " SEK konverteras till: ");
+        System.out.printf(format2_3, euro,"EUR");
+        System.out.printf(format2_3, dollar,"USD");
+*/
+        // Övning 3.1
+
+        System.out.println("Ålderskontroll");
+        System.out.println("Ange er ålder: ");
+
+        int age3_1 = scanner.nextInt();
+        System.out.println("Okej, om ni är " + age3_1 + " så får ni: ");
+        if (age3_1 >= 18) {
+            System.out.println("köra bil!");
+            }
+        else {
+            System.out.println("INTE köra bil!");
+        }
+        if (age3_1 >= 20) {
+            System.out.println("köpa alkohol!");
+        }
+        else {
+            System.out.println("INTE köpa alkohol!");
+        }
 
 
     }
