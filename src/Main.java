6f9b7155712bfd782c2cfae72ba295b84fbb5795
Gabriel Note <gabriel.emil.note@gmail.com>
2025-09-01 13:40:14 +0200

    Övning 6.1

--- src/Main.java
+++ src/Main.java
@@ -218,9 +218,9 @@ public class Main {
             System.out.println("4 för att summera alla tal från 1 till 100");
             System.out.println("0 för att avsluta programmet");
 
-            int selection = scanner.nextInt();
+            int selection5_1 = scanner.nextInt();
             int k;
-            switch (selection) {
+            switch (selection5_1) {
                 case 0:
                     System.out.println("Tack för att ni använde räknaren");
                     loopCheck = false;
@@ -293,9 +293,52 @@ public class Main {
         }
 
 
-         */
+
 
         // Övning 6.1
 
+        System.out.println("Välkommen, Gör ett val för att komma vidare");
+        boolean loopCheck6_1 = true;
+        while (loopCheck6_1) {
+            System.out.println(" ");
+            System.out.println("1 för att räkna ut arean av en rektangel");
+            System.out.println("2 för att räkna ut arean av en cirkel");
+            System.out.println("3 för att avsluta programmet");
+            int selection6_1 = scanner.nextInt();
+            double result6_1 = 0;
+            switch (selection6_1) {
+                case 1:
+                    System.out.println("Ange första sidans längd");
+                    double sideA = scanner.nextDouble();
+                    System.out.println("Ange andra sidans längd");
+                    double sideB = scanner.nextDouble();
+                    result6_1 = sideA * sideB;
+                    System.out.println("Arean är " + result6_1 + " (" + sideA + " * " + sideB + ")");
+                    break;
+                case 2:
+                    System.out.println("Ange cirkelns radie");
+                    double radius6_1 = scanner.nextDouble();
+                    result6_1 = Math.PI * radius6_1 * radius6_1;
+                    System.out.println("Arean av cirkeln är " + result6_1);
+                    break;
+                case 3:
+                    System.out.println("Tack för att ni använde vår areaberäknare");
+                    loopCheck6_1 = false;
+                    break;
+            }
+        }
+
+         */
+
+        // Övning 6.2
+
+
+
     }
 }
+
+
+
+
+
+
