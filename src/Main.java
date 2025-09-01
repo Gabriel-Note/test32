9e946004d45868720b6f457b9c5cc4e993f67c9a
Gabriel Note <gabriel.emil.note@gmail.com>
2025-09-01 11:03:16 +0200

    Övning 5.1

--- src/Main.java
+++ src/Main.java
@@ -174,7 +174,7 @@ public class Main {
             System.out.println("Fel användarnamn eller lösenord");
         }
 
-         */
+
 
         //Övning 4.2
 
@@ -203,6 +203,67 @@ public class Main {
         System.out.printf(format4_2, "Originalpris:",price4_2 + " kr");
         System.out.printf(format4_2,"Rabatt:", discount4_2 + "%");
         System.out.printf(format4_2,"Rabatterat pris:", finalPrice4_2 + " kr");
+
+
+
+        // Övning 5.1
+
+        boolean loopCheck = true;
+        while (loopCheck){
+            System.out.println("Ange ett nummer för vad ni vill göra");
+            System.out.println("1 för att räkna från 1 till 10");
+            System.out.println("2 för att räkna från 10 till 1");
+            System.out.println("3 för att räkna alla jämna tal från 2 till 20");
+            System.out.println("4 för att summera alla tal från 1 till 100");
+            System.out.println("0 för att avsluta programmet");
+
+            int selection = scanner.nextInt();
+            int k;
+            switch (selection) {
+                case 0:
+                    System.out.println("Tack för att ni använde räknaren");
+                    loopCheck = false;
+                    break;
+                case 1:
+                    System.out.println("Du valde nummer 1");
+                    k = 1;
+                    while (k <= 10) {
+                        System.out.println(k);
+                        k++;
+                    }
+                    break;
+                case 2:
+                    System.out.println("Du valde nummer 2");
+                    k = 10;
+                    while (k >= 1) {
+                        System.out.println(k);
+                        k--;
+                    }
+                    break;
+                case 3:
+                    System.out.println("Du valde nummer 3");
+                    for (k = 2; k <= 20; k += 2){
+                        System.out.println(k);
+                    }
+                    break;
+                case 4:
+                    System.out.println("Du valde nummer 4");
+                    int sum = 0;
+                    for (k = 1; k <= 100; k++){
+                        sum += k;
+                    }
+                    System.out.println("Totala summan är " + sum);
+                    break;
+                default:
+                    System.out.println("Ogiltigt val!");
+            }
+
+        }
+
+         */
+
+        // Övning 5.2
+
 
 
 
