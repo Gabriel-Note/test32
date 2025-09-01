5cbd9e68e7e8e5c0c00fa3c3c5e7e26b98fafd3f
Gabriel Note <gabriel.emil.note@gmail.com>
2025-09-01 15:27:41 +0200

    Startat på program A och gjort tom fråga 3

--- src/Main.java
+++ src/Main.java
@@ -330,7 +330,7 @@ public class Main {
             }
         }
 
-         */
+
 
         // Övning 6.2
 
@@ -343,6 +343,92 @@ public class Main {
             age6_2 = scanner.nextInt();
         } while (age6_2 < 1 || age6_2 > 120);
         System.out.println("Er ålder är " + age6_2);
+
+         */
+
+        // Program A: Interaktiv quiz
+
+        System.out.println("Välkommen till den triviala quizzen");
+        boolean loopCheck = true;
+        int currentAnswer;
+        int correctAnswer1 = 2;
+        int correctAnswer2 = 1;
+        int correctAnswer3 = 4;
+        int correctAnswer4 = 1;
+        int correctAnswer5 = 2;
+        int numberOfCorrectAnswers = 0;
+        // Fråga  1
+        System.out.println("vad är 1+4?");
+        System.out.println(" ");
+        System.out.println("1:    4");
+        System.out.println("2:    5");
+        System.out.println("3:    14");
+        System.out.println("4:    1");
+        while (loopCheck) {
+            currentAnswer = scanner.nextInt();
+            switch (currentAnswer){
+                case 1, 3, 4:
+                    System.out.println("Tyvärr, nummer " + correctAnswer1 + " var rätt svar");
+                    loopCheck = false;
+                    break;
+                case 2:
+                    numberOfCorrectAnswers += 1;
+                    System.out.println("Rätt svar");
+                    loopCheck = false;
+                    break;
+                default:
+                    System.out.println("Ogiltigt val, försök igen");
+            }
+        }
+        // Fråga 2
+        System.out.println("Vad är 4/0");
+        System.out.println(" ");
+        System.out.println("1:    Det går inte");
+        System.out.println("2:    0");
+        System.out.println("3:    Oändligt");
+        System.out.println("4:    40");
+        loopCheck = true;
+        while (loopCheck) {
+            currentAnswer = scanner.nextInt();
+            switch (currentAnswer){
+                case 2, 3, 4:
+                    System.out.println("Tyvärr, nummer " + correctAnswer2 + " var rätt svar");
+                    loopCheck = false;
+                    break;
+                case 1:
+                    numberOfCorrectAnswers += 1;
+                    System.out.println("Rätt svar");
+                    loopCheck = false;
+                    break;
+                default:
+                    System.out.println("Ogiltigt val, försök igen");
+            }
+        }
+        // Fråga 3
+        System.out.println("Vad är 7*8");
+        System.out.println(" ");
+        System.out.println("1:    49");
+        System.out.println("2:    63");
+        System.out.println("3:    78");
+        System.out.println("4:    56");
+        loopCheck = true;
+        while (loopCheck) {
+            currentAnswer = scanner.nextInt();
+            switch (currentAnswer){
+                case 1, 2, 3:
+                    System.out.println("Tyvärr, nummer " + correctAnswer3 + " var rätt svar");
+                    loopCheck = false;
+                    break;
+                case 4:
+                    numberOfCorrectAnswers += 1;
+                    System.out.println("Rätt svar");
+                    loopCheck = false;
+                    break;
+                default:
+                    System.out.println("Ogiltigt val, försök igen");
+            }
+        }
+
     }
 }
 
